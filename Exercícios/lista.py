ba5e387c3b47a20b9babdc5472f75e1b6cbe4b02
Caliel Lessa <caliel.muniz@hotmail.com>
2025-05-29 12:19:56 -0300

    Lista de Exercícios

---
+++ Exercícios/lista.py
@@ -0,0 +1,78 @@
+# def parImpar(num):
+#   if num %2 == 0:
+#     return "esse número é par"
+#   else:
+#     return "esse número é impar"
+
+# def maiorMenor (num1, num2):
+#   if num1 > num2:
+#     print(f'{num1} é maior que {num2}')
+#   elif num2 > num1:
+#     print(f'{num2} é maior que {num1}')
+#   else:
+#     print(f'{num2} é igual a {num1}')
+
+# def contador (num3):
+#   listC = []
+#   i = 1
+#   listC.append(i)
+#   while i < num3:
+#     i+= 1
+#     listC.append(i)
+#   return listC
+
+# def somador (*dados):
+#   r = 0
+#   for i in dados:
+#      r += i
+#   return r
+
+# def tabuada (n1):
+#   for i in range(10):
+#     print(f'{i+1} X {n1}: {n1*(i+1)}')
+
+# def palindromo(palavra):
+#   palavraN = []
+#   for c in reversed(palavra):
+#     palavraN.append(c)
+#   palavraN = ''.join(palavraN)
+#   print(palavraN.lower())
+#   if  palavraN.lower() == palavra.lower():
+#     return 'essa palavra é um palíndromo'
+#   else:
+#     return 'essa palavra não é um palíndromo'
+
+
+    
+
+
+  
+
+# numPI = int(input("informe um número: "))
+# print(parImpar(numPI))
+
+# num1 = int(input("informe um número: "))
+# num2 = int(input("informe um número: "))
+# print(maiorMenor(num1, num2))
+
+# num3 = int(input("informe um número: "))
+# print(contador(num3))
+
+# n1 = int(input("informe um número: "))
+# n2 = int(input("informe um número: "))
+# n3 = int(input("informe um número: "))
+# n4 = int(input("informe um número: "))
+# n5 = int(input("informe um número: "))
+# print(somador(n1,n2,n3,n4,n5))
+
+# numT= int(input("informe um número: "))
+# print(tabuada(numT))
+
+# palavra = input("informe uma palavra: ")
+# print(palindromo(palavra))
+
+
+
+
+
+
